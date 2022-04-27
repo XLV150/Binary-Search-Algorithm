9c318110f70874129e0d0945fbe0c91546ac5fc8
Kieran <98961254+XLV150@users.noreply.github.com>
2022-04-27 16:48:01 +0200

    Update Binary search algorithm.py

--- Binary search algorithm.py
+++ Binary search algorithm.py
@@ -3,7 +3,7 @@ high = 1000
 
 #### NOTE ####
 
-# CHOOSE YOUR NUMBER AND ENTER IT.
+# CHOOSE YOUR NUMBER AND DONT FORGET IT.
 # THE PROGRAM WILL GUESS A NUMBER, IF ITS TOO HIGH YOU SIMPLY ENTER l, for lower
 # IF ITS TOO LOW, ENTER h, for higher
 # AND ITS ITS CORRECT, ENTER c
@@ -17,7 +17,7 @@ print()
 
 guesses = 1
 while True:
-    print("\tGuessing in the range of {} and {}.".format(low, high))
+    print("Guessing in the range of {} and {}.".format(low, high))
     guess = low + (high - low) // 2
     high_low = input("The computers guess is {}. Should it guess h or l, or c if it was correct. ".format(guess)).casefold()
 
